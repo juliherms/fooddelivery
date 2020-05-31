595a150741ac6195d8c260c12f2b78ad8c43111b
Vasconcelos <j.a.vasconcelos@accenture.com>
2020-05-31 14:53:53 -0300

    create a eureka server

---
+++ eureka-server/src/main/java/com/food/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,20 @@
+package com.food.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Eureka service discovery
+ * @author j.a.vasconcelos
+ *
+ */
+@EnableEurekaServer
+@SpringBootApplication
+public class EurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaServerApplication.class, args);
+	}
+
+}
--- products-service/src/main/java/com/food/productsservice/ProductsServiceApplication.java
+++ products-service/src/main/java/com/food/productsservice/ProductsServiceApplication.java
@@ -2,8 +2,15 @@ package com.food.productsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
+/**
+ * Microservice responsible to persist products in the application
+ * @author j.a.vasconcelos
+ *
+ */
 @SpringBootApplication
+@EnableEurekaClient //allow service discovery
 public class ProductsServiceApplication {
 
 	public static void main(String[] args) {
